25e28ae00ac4762c48ddb43828306c21aa017bd9
Karen <ksuen.dev@gmail.com>
2022-02-05 00:42:01 +0000

    test(button): revise tests for button text and colour

--- tests/unit/components/button.spec.js
+++ tests/unit/components/button.spec.js
@@ -2,19 +2,32 @@ import { shallowMount } from '@vue/test-utils'
 import Button from '@/components/Button'
 
 describe('Button', () => {
-  it('renders props.msg when passed', () => {
+  it('Button text is rendered correctly with the text passed in from props', () => {
     const text = "Hello";
-    const color = "green";
 
     const wrapper = shallowMount(Button, {
       props: {
         text: text,
+        color: "",
+      }
+    })
+
+    expect(wrapper.find('button').exists());
+    expect(wrapper.text()).toMatch(text);
+  }),
+
+  it('Button colour is rendered correctly with the color passed in from props', () => {
+    const color = "green";
+
+    const wrapper = shallowMount(Button, {
+      props: {
+        text: "",
         color: color,
       }
     })
-    expect(wrapper.text()).toMatch(text);
-    expect(wrapper.classes()).toContain('btn');
-    expect(wrapper.find('button').element.style.color).toBe(color);
+
     expect(wrapper.find('button').exists());
+    expect(wrapper.classes()).toContain('btn');
+    expect(wrapper.element.style.background).toBe(color);
   })
 })
